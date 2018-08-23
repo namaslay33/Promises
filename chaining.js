371618771852730a9de6c852d7feff8ccf64d0b5
Namaslay33 <skylar.hunt@utexas.edu>
2018-08-23 15:59:19 -0500

    Chained Two Promises Together

---
+++ chaining.js
@@ -0,0 +1,33 @@
+let express = require('express');
+let app = express();
+var http = require('http');
+var rp = require('request-promise');
+var fs = require('fs');
+
+
+url = 'http://www.issarae.com';
+filename = 'issa.txt';
+
+var saveWebPage = (url, filename) => {
+
+ promise1 = rp(url); 
+ promise2 = filename;
+
+   
+    promise1.then(function(promise1){
+        fs.writeFile(promise2, promise1, function(error){
+            if(error) throw error
+        })
+        
+    });
+    
+
+   
+};
+
+saveWebPage(url, filename);
+
+
+app.listen(8000, function(){
+    console.log('Listening on port 8000');
+});
